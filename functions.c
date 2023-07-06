1da70c3df1a2b0711e8112a094913a6799dfef75
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-06 12:45:05 -0700

    Setting up functions file

---
+++ functions.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * print_character - Prints a character to the output
+ * @varg: Arguments used in the function
+ * Return: Returns a count to the main function
+ */
+
+int print_char(va_list varg)
+{
