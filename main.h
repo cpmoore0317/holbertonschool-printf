6a156454f84577164dbcae59baba0f41f708c395
Zal-atan <ethanzalta@yahoo.com>
2023-07-06 10:00:00 -0700

    Adding struct for pointer functions and putchar to main.h

--- main.h
+++ main.h
@@ -5,7 +5,14 @@
 #include <unistd.h>
 #include <stdio.h>
 
+typedef struct form
+{
+	char type;
+	void (*f)(va_list);
+} type_f;
+
 /* Prototypes */
+int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
