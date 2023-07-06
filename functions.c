388bd6290449238ae4a036185d4de9c8b2acded8
Zal-atan <ethanzalta@yahoo.com>
2023-07-06 12:57:31 -0700

    made basic forms for helper functions

--- functions.c
+++ functions.c
@@ -1,10 +1,28 @@
 #include "main.h"
 
 /**
- * print_character - Prints a character to the output
+ * print_char - Prints a character to the output
  * @varg: Arguments used in the function
  * Return: Returns a count to the main function
  */
-
 int print_char(va_list varg)
 {
+}
+
+/**
+ * print_string - Prints a string to the output
+ * @varg: Arguments used in function
+ * Returns a count to the main function
+ */
+ int print_string(va_list varg)
+ {
+ }
+
+/**
+ * print_integer - Prints a string to the output
+ * @varg: Arguments used in function
+ * Returns a count to the main function
+ */
+ int print_integer(va_list varg)
+ {
+ }
