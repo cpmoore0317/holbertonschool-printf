e4cc1c517cb4758621ed2174d73bb29f417adf2e
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-08 09:39:08 -0700

    Fixing spaces for betty

--- functions.c
+++ functions.c
@@ -9,17 +9,17 @@ int print_edge(char e)
 	if (e == '\0')
 		return (-1);
 	else if (e == '%')
-    {
+	{
 		_putchar('%');
 		return (1);
-    }
+	}
 	else
-    {
+	{
 		_putchar('%');
 		_putchar(e);
 		return (2);
-    }
-    return (0);
+	}
+	return (0);
 }
 
 /**
@@ -29,10 +29,10 @@ int print_edge(char e)
  */
 int print_char(va_list varg)
 {
-    char c = va_arg(varg, int);
+	char c = va_arg(varg, int);
 
-    _putchar(c);
-    return (1);
+	_putchar(c);
+	return (1);
 }
 
 /**
@@ -42,21 +42,21 @@ int print_char(va_list varg)
  */
 int print_string(va_list varg)
 {
-    char *str = va_arg(varg, char *);
-    int i = 0, count = 0;
+	char *str = va_arg(varg, char *);
+	int i = 0, count = 0;
 
-    if (str == NULL)
-    {
-        str = "(null)";
-    }
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
 
-    while (str[i])
-    {
-        _putchar(str[i]);
-        i++;
-        count++;
-    }
-    return (count);
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+		count++;
+	}
+	return (count);
 }
 
 /**
@@ -66,24 +66,24 @@ int print_string(va_list varg)
  */
 int print_integer(va_list varg)
 {
-    long int num = va_arg(varg, long int);
-    int x = 1, temp, count = 0;
+	long int num = va_arg(varg, long int);
+	int x = 1, temp, count = 0;
 
-    while ((num / x) > 10 )
-        x *= 10;
+	while ((num / x) > 10 )
+		x *= 10;
 
-    for (; x >= 1; x /= 10)
-    {
-        temp = (num / x);
-        _putchar(temp + 48);
-        temp *= x;
-        if (temp != 0)
-        {
-            num = num % temp;
-        }
-        count += 1;
-        if (x == 1)
-        return (count);
-    }
-    return (count);
+	for (; x >= 1; x /= 10)
+	{
+		temp = (num / x);
+		_putchar(temp + 48);
+		temp *= x;
+		if (temp != 0)
+		{
+			num = num % temp;
+		}
+		count += 1;
+		if (x == 1)
+			return (count);
+	}
+	return (count);
 }
